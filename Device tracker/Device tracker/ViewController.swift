0c0d36d728fa50376ad32f8d18ea35cf311f85a6
Jaewoo Cho <59891471+mikael1017@users.noreply.github.com>
2020-10-15 20:39:21 -0700

    Device Tracker app

---
+++ Device tracker/Device tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Device tracker
+//
+//  Created by Jaewoo Cho on 10/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
