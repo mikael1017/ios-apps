80ba2bfb7a3fb03d9693f4bc1ab87639c9470b63
mikael1017 <59891471+mikael1017@users.noreply.github.com>
2020-10-26 20:41:08 -0700

    Fixed unwrapper

--- Device tracker/Device tracker/ViewController.swift
+++ Device tracker/Device tracker/ViewController.swift
@@ -9,14 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var selectedDevice : Device?
+    var selectedDevice : Device? = nil
     @IBOutlet weak var imageField: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = selectedDevice?.name
-        if let imageData = selectedDevice?.image {
-            imageField?.image = UIImage(data: imageData)
+        if let selectedDevice = selectedDevice {
+            let deviceQuantity = "\(selectedDevice.quantity)"
+            let deviceName = selectedDevice.name
+            title = deviceName! + " - total: " + deviceQuantity + " devices"
+            if let imageData = selectedDevice.image {
+                imageField?.image = UIImage(data: imageData)
+            }
         }
     }
 
